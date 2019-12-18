c734c8300b289c633ae72045af001a717fe1a293
HandsomeLuoyang <52132083+HandsomeLuoyang@users.noreply.github.com>
2019-12-18 10:16:30 +0800

    Connect the login_window and the main_window.Set two accounts and jiami the password

--- login_window.py
+++ login_window.py
@@ -4,6 +4,8 @@ from PyQt5.QtGui import QIcon, QMouseEvent, QPixmap
 from PyQt5 import Qt
 import sys
 from Crypto.Cipher import AES
+from main_window import Main_window
+from 功能.jiami import PrpCrypt
 
 
 class Main(Ui_Form):
@@ -24,8 +26,29 @@ class Main(Ui_Form):
             QMessageBox.warning(self.qwidget, "温馨提示！", "请输入账户名和密码！")
             self.Username.setFocus()
             return
-        else:
-            main_window.main()
+
+        with open("account.txt", "r") as rf:
+            account_list = rf.readlines()
+        user_index = -1
+        for i in range(0, len(account_list), 2):
+            if userName == account_list[i][9:].rstrip():
+                user_index = i
+                break
+        if user_index == -1:
+            QMessageBox.warning(self.qwidget, "提示", "账户名或密码错误！")
+            self.Password.setText("")
+            return False
+        real_pwd = account_list[user_index+1][9:].rstrip()
+        pc = PrpCrypt()
+        pwd = pc.encrypt(passWord).decode("utf-8")
+        if real_pwd != pwd:
+            QMessageBox.warning(self.qwidget, "提示", "账户名或密码错误！")
+            self.Password.setText("")
+            return False
+
+        self.main_window = Main_window()
+        self.main_window.show()
+        self.qwidget.hide()
 
 
 def main():
